cb02c153783d95a094a91a182a65a46fd06fb6bc
Mohammad <saif_kbr@yahoo.co.in>
2022-10-04 23:57:47 +0100

    Solution commit

---
+++ RomanNumeralsApp.UnitTest/ConverterTest.cs
@@ -0,0 +1,48 @@
+﻿using RomanNumeralsTestApp;
+
+namespace RomanNumeralsApp.UnitTest;
+
+public class ConverterTest
+{
+    [Theory]
+    [InlineData(10)]
+    public void Convert_ValidIntegerInput_ShouldReturnString(int number)
+    {
+        // Arrange
+        IntToRomanConverter converter = new IntToRomanConverter();
+
+        // Act
+        string result = converter.Convert(number);
+
+        // Assert
+        Assert.IsType<string>(result);
+    }
+
+    [Theory]
+    [InlineData(2, "II")]
+    [InlineData(5, "V")]
+    [InlineData(1999, "MCMXCIX")]
+    public void Convert_Integer_ToRomanNumeral(int number, string expectedRomanNumeral)
+    {
+        // Arrange
+        IntToRomanConverter converter = new IntToRomanConverter();
+
+        // Act
+        string actualRomanNumeral = converter.Convert(number);
+
+        // Assert
+        Assert.Equal(expectedRomanNumeral, actualRomanNumeral);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2000)]
+    public void Convert_ThrowsExceptionIfNotFound(int number)
+    {
+        // Arrange
+        IntToRomanConverter converter = new IntToRomanConverter();
+
+        // Act
+        Assert.Throws<Exception>(() => converter.Convert(number));
+    }
+}
---
+++ RomanNumeralsTestApp/IntToRomanConverter.cs
@@ -0,0 +1,44 @@
+﻿namespace RomanNumeralsTestApp;
+
+public class IntToRomanConverter
+{
+    private const int MinIntRange = 1;
+    private const int MaxIntRange = 2000;
+
+    public string Convert(int number)
+    {
+        Dictionary<string, int> romanNumbersDictionary = new()
+        {
+            {"I", 1},
+            {"IV", 4},
+            {"V", 5},
+            {"IX", 9},
+            {"X", 10},
+            {"XL", 40},
+            {"L", 50},
+            {"XC", 90},
+            {"C", 100},
+            {"CD", 400},
+            {"D", 500},
+            {"CM", 900},
+            {"M", 1000}
+        };
+
+        if (number > MinIntRange && number < MaxIntRange)
+        {
+            string romanRepresentation = "";
+            foreach (var item in romanNumbersDictionary.Reverse())
+            {
+                while (number >= item.Value)
+                {
+                    romanRepresentation += item.Key;
+                    number -= item.Value;
+                }
+            }
+            return romanRepresentation;
+        }
+
+        throw new Exception("Invalid input");
+    }
+}
+
